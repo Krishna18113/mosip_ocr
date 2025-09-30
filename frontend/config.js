efb982468b535d1339ce1056ab501b1dfc1bdccd
Krishna18113 <gattukrishnavenkatsai@gmail.com>
2025-09-30 17:37:44 +0530

    backend_url update

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,4 @@
-const BACKEND_URL = 'https://unmakable-sommer-unpersuasive.ngrok-free.dev';
+const BACKEND_URL = 'http://127.0.0.1:8000';
+
+// const BACKEND_URL = 'https://unmakable-sommer-unpersuasive.ngrok-free.dev';
 // const BACKEND_URL = 'https://headlong-gentler-ta.ngrok-free.dev';
